28f9e0e46d9b84157a558a6875c201ae80f9828b
Colin Woodbury <colin@fosskers.ca>
2020-08-07 11:13:57 -0700

    [haskell] JSON server with `wai`

---
+++ simple-json/haskell-wai/src/Main.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE DeriveAnyClass     #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE OverloadedStrings  #-}
+
+module Main where
+
+import           Data.Aeson (FromJSON, ToJSON, decode', encode)
+import           Data.Text (Text)
+import           Data.Time.Clock (UTCTime)
+import           GHC.Generics (Generic)
+import           Network.HTTP.Types
+import           Network.Wai
+import qualified Network.Wai.Handler.Warp as W
+
+data Colour = Red | Green | Blue
+  deriving stock (Generic)
+  deriving anyclass (FromJSON, ToJSON)
+
+data User = User
+  { name      :: Text
+  , age       :: Word
+  , colour    :: Colour
+  , numbers   :: [Int]
+  , timestamp :: UTCTime
+  , missing   :: Maybe Bool }
+  deriving stock (Generic)
+  deriving anyclass (FromJSON, ToJSON)
+
+app :: Application
+app = routes
+  where
+    routes :: Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived
+    routes req resp = case pathInfo req of
+      [] -> handleUser req >>= resp
+      _  -> resp $ responseLBS status404 headers "Not Found"
+
+    headers = [("Content-Type", "text/html")]
+
+handleUser :: Request -> IO Response
+handleUser req = case requestMethod req of
+  "POST" -> do
+    body <- strictRequestBody req
+    pure $ case decode' body of
+      Nothing   -> responseLBS status400 [] "Unable to parse JSON"
+      Just user -> responseLBS status200 headers . encode $ tweakUser user
+  _ -> pure $ responseLBS status404 [] ""
+  where
+    headers = [("Content-Type", "application/json")]
+
+tweakUser :: User -> User
+tweakUser u = u { age = age u + 1 }
+
+main :: IO ()
+main = W.run 8080 app
