66c6d2d7440dfba97661389c530bcccea3e4351c
Colin Woodbury <colin@fosskers.ca>
2020-08-18 14:03:03 -0700

    [rust] Use `rustqlite` instead
    While managing its connection pool via `r2d2`. So far it's performing
    well, and I'm not getting any weird panics like I was with `sqlx`.

--- database/rust-tide/src/main.rs
+++ database/rust-tide/src/main.rs
@@ -1,48 +1,59 @@
 use chrono::prelude::*;
+use r2d2::Pool;
+use r2d2_sqlite::SqliteConnectionManager;
+use rusqlite::params;
 use serde::Serialize;
-use sqlx::SqlitePool;
 use tide::{Body, Request};
 
-#[derive(Serialize, sqlx::Type)]
-#[repr(i32)]
-enum Colour {
-    Red = 0,
-    Green = 1,
-    Blue = 2,
-}
+// #[derive(Serialize)]
+// #[repr(i32)]
+// enum Colour {
+//     Red = 0,
+//     Green = 1,
+//     Blue = 2,
+// }
 
-#[derive(Serialize, sqlx::FromRow)]
+#[derive(Serialize)]
 struct User {
     name: String,
     age: i32,
     profile: String,
-    colour: Colour,
-    // timestamp: DateTime<Utc>,
+    // colour: Colour,
+    timestamp: DateTime<Utc>,
     missing: Option<bool>,
 }
 
 #[derive(Clone)]
 struct State {
-    pool: SqlitePool,
+    pool: Pool<SqliteConnectionManager>,
 }
 
 #[async_std::main]
 async fn main() -> anyhow::Result<()> {
     let state = {
-        let pool = SqlitePool::connect("../test.db").await?;
+        let m = SqliteConnectionManager::file("../test.db");
+        let pool = Pool::new(m)?;
         State { pool }
     };
 
     let mut app = tide::with_state(state);
     app.at("/:name").get(|req: Request<State>| async move {
+        let conn = req.state().pool.get()?;
         let name: String = req.param("name")?;
-        let pool = &req.state().pool;
-        let user = sqlx::query_as::<_, User>("SELECT * from test where name = ? limit 1")
-            .bind(name)
-            .fetch_one(pool)
-            .await?;
+        // TODO Try `prepare_cached` too.
+        let mut stmt = conn.prepare("SELECT * FROM test WHERE name = ? limit 1")?;
+        let user = stmt.query_row(params![name], |row| {
+            let user = User {
+                name: row.get(0)?,
+                age: row.get(1)?,
+                profile: row.get(2)?,
+                timestamp: row.get(4)?,
+                missing: row.get(5)?,
+            };
+            Ok(user)
+        })?;
 
-        Ok(Body::from_json(&user)?)
+        Ok(Body::from_json(&user).expect("Json encoding failed"))
     });
     app.listen("127.0.0.1:8080").await?;
 
